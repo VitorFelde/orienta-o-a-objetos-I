d94b8cc4aebf7aa923d5662256e7ce7afe4bf3ad
Vitor Felde <feldevitor@gmail.com>
2026-08-10 21:13:04 -0300

    Add files via upload

---
+++ introducao ao java/Exemplo5.java
@@ -0,0 +1,15 @@
+public class Exemplo5 {
+	public static void main (String args []) { 
+		
+		OperacoesMatematicas opMat = new OperacoesMatematicas();
+		
+		System.out.println ("67 * 44 = " + opMat.multiplicar (67, 44));
+		
+		System.out.println ("22 / 8 = " + opMat.dividir (22, 8));
+		
+		System.out.println ("25 + 33 = " + opMat.somar (25, 33));
+		
+		System.out.println ("41 - 25 = " + opMat.subtrair (41, 25));
+		
+		}
+}
