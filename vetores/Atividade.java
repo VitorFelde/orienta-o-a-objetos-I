39b47836e86c14bc1b3d4dc9b3656d24fea5499f
Vitor Felde <feldevitor@gmail.com>
2026-08-26 10:05:02 -0300

    Update Atividade.java

--- vetores/Atividade.java
+++ vetores/Atividade.java
@@ -16,21 +16,38 @@ Ex: Se vetA[0] tiver o valor 4 e vetB[0] tiver a palavra "java", escreve "java" 
     
 public static void main(String[] args) throws Exception {
 
-BufferedReader reader = new BufferedReader(
+BufferedReader reader = new BufferedReader( //criamos o objeto de leitura que vamos usar posteriormente top
     new InputStreamReader(System.in));
     
-    int tamanhoVetor = 4;
+    int tamanhoVetor = 4; //ideia top pra nao usar valor fixo toda vez, boa pratica de programacao
     int vetA[] = new int[tamanhoVetor];
     String vetB[] = new String[tamanhoVetor];
 
     System.out.println("Digite quatro valores para o vetA: ");
     for (int i = 0; i < tamanhoVetor; i++) {
         vetA[i] = Integer.parseInt(reader.readLine());
+        /*o Integer.parseInt pega a string e converte em int
+        o reader.readLine serve para leitura, vai ser sempre assim a sintaxe*/
     }
     
     System.out.println("Digite quatro nomes para o vetB: ");
     for (int i = 0; i < tamanhoVetor; i++) {
-        vetB[i] = reader.readLine();
+        vetB[i] = reader.readLine(); 
+        /*aqui queremos ler a string mesmo entao na fazemos nada]
+        dai armazenamos na posicao i do vetB tudo que o usuario digitar
+        por isso tem esse for topzaço*/
+    }
+    
+    for (int i = 0; i < tamanhoVetor; i++) {
+        for (int j = 0; j < vetA[i]; j++) {
+        /*aqui criamos outro for para poder percorrer o valor 
+        que o usuario vai digitar, se fosse fazer so um for
+        colocando como limite o tamanhoVetor, ele sempre mostraria
+        as palavras 4 vezes, dessa forma, pegamos o numero na posicao i do vetA
+        e dizemos ao programa para printar esse numero de vezes*/
+        System.out.println (vetB[i]);
+            
+        }
     }
 }
 
