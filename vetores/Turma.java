4f80f688adf795f4899db861f437603be93f4465
Vitor Felde <feldevitor@gmail.com>
2026-09-01 11:38:09 -0300

    Correct comments
    Added comments for clarity on attributes and methods.

--- vetores/Turma.java
+++ vetores/Turma.java
@@ -1,21 +1,25 @@
 public class Turma {
-    // Atributos declarados
+    // Atributos declarados topzao
     private int nroTurma;
     private String nomeCurso;
     private int anoIngresso;
     private int qtdeAlunos;
-    private Aluno[] vetAlunos;
+    private Aluno[] vetAlunos; //isso aqui quer dizer que vamos criar um vetor que entram alunos que vao ter que preencher todos os dados 
+    //solicitados no arquivo aluno
 
 
     public Turma (){
         this.vetAlunos = new Aluno[40]; 
+        /*aqui estamos avisando o java que no vetor Aluno só podem entrar até 40 alunos*/
     }
 
     public int getNroTurma() {
-        return this.nroTurma;
+        return this.nroTurma; 
     }
 
     public void setNroTurma(int nroTurma) {
+        /*criamos uma variavel com nome nroTurma de tipo int para poder passar valor depois na main,
+        e por isso usamos o this embaixo para que o codigo saiba que queremos salvar o valor no nroTurma do escopo topzeira*/
         this.nroTurma = nroTurma;
     }
 
@@ -40,11 +44,11 @@ public class Turma {
     }
 
     public Aluno getAluno(int pos){
-        return this.vetAlunos[pos]; //criamos pos para melhor controle
+        return this.vetAlunos[pos]; //criamos pos para poder puxar o aluno pela posição que queremos
     }
 
     public void setAlunos(Aluno a) {
-        if (this.qtdeAlunos < 40) {
+        if (this.qtdeAlunos < 40) { //enquanto for menor que 40 podemos cadastrar alunos
         this.vetAlunos[qtdeAlunos] = a;
         qtdeAlunos++;
         }
